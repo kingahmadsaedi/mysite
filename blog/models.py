9d80bfe8b800754de4277b19b2f9ab5c9c3b414a
kingahmadsaedi <ahmadsaedilinux@gmail.com>
2025-11-06 20:36:39 +0330

    create migrations
    update migrations

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    # image
+    # auther
     title=models.CharField(max_length=255)
     content = models.TextField()
+    # tag
+    # category
+    counted_view= models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    publish_date=models.DateTimeField(null=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+    update_date=models.DateTimeField(auto_now=True)
+
+
+
+    
