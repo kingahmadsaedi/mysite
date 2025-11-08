31e0bd6a97eb14235837ae91727c39f603be736b
kingahmadsaedi <ahmadsaedilinux@gmail.com>
2025-11-08 22:58:45 +0330

    add post from admin panel
    edit admin.py on blog app
    edit models on blog app

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,9 @@ class Post(models.Model):
     publish_date=models.DateTimeField(null=True)
     created_date=models.DateTimeField(auto_now_add=True)
     update_date=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.title} Post Number {self.id}'
 
 
 
