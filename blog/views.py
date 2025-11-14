970ae9ddaafaa55426c5d50d2f5ba5a2127a6a15
kingahmadsaedi <ahmadsaedilinux@gmail.com>
2025-11-14 22:19:46 +0330

    edit single post
    update single post with new data from database

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 # Create your views here.
 def blog_view(request):
     posts=Post.objects.filter(status=1)
     context={'posts':posts}
     return render(request,'blog/blog-home.html',context)
-def blog_single(request):
-    return render(request,'blog/blog-single.html')
+def blog_single(request,pid):
+    post=get_object_or_404(Post,pk=pid)
+    context={'post':post}
+    return render(request,'blog/blog-single.html',context)
 
+def test_view(request,pid):
+    #post=Post.objects.get(id=pid)
+    post=get_object_or_404(Post,pk=pid)
+    context={'post':post}
+    return render(request,'blog/test-view.html',context)
