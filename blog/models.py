3065cd7d56010648e5cdf4af52cdbfc300e9f3d6
kingahmadsaedi <ahmadsaedilinux@gmail.com>
2025-11-16 18:33:55 +0330

    add category
    add category to post

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 class Post(models.Model):
     image=models.ImageField(upload_to = 'blog/',default='blog/default.jpg')
     auther=models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
     title=models.CharField(max_length=255)
     content = models.TextField()
     # tag
-    # category
+    category=models.ManyToManyField(Category)
     counted_view= models.IntegerField(default=0)
     status=models.BooleanField(default=False)
     publish_date=models.DateTimeField(null=True)
