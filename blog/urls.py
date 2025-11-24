e0db461d4bd4b21ef3d2a1025714ca0c30b3725f
kingahmadsaedi <ahmadsaedilinux@gmail.com>
2025-11-24 22:32:40 +0330

    serarch and pagination
    create blog search on view
    create pagination

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<int:pid>',blog_single,name='single'),
     path('category/<str:cat_name>',blog_view,name='category'),
     path('auther/<str:auther_username>',blog_view,name='auther'),
+    path('search/',blog_search,name='search'),
     path('test',test_view,name='test')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from unicodedata import category
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 def blog_view(request,**kwargs):
     posts=Post.objects.filter(status=1)
-    print(kwargs)
     if kwargs.get('cat_name') !=None:
         posts=posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('auther_username') !=None:
         posts=posts.filter(auther__username=kwargs['auther_username'])
+    posts=Paginator(posts,3)
+    try:
+        page_number=request.GET.get('page')
+        posts=posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts=posts.get_page(1)
+    except EmptyPage:
+        posts=posts.get_page(1)
     context={'posts':posts}
     print(context)
     return render(request,'blog/blog-home.html',context)
@@ -17,7 +25,13 @@ def blog_single(request,pid):
     post=get_object_or_404(posts,pk=pid)
     context={'post':post}
     return render(request,'blog/blog-single.html',context)
-
+def blog_search(request):
+    posts=Post.objects.filter(status=1)
+    if request.method=='GET':
+        if s:=request.GET.get('s'):
+            posts=posts.filter(content__contains=s)
+    context={'posts':posts}
+    return render(request,'blog/blog-home.html',context)
 def test_view(request):
     
     #post=Post.objects.get(id=pid)
